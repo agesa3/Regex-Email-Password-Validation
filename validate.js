cf93174a1a4a96990955a71e2d5874b37a5868c0
agesa3 <collinsagesa2000@gmail.com>
2021-09-23 01:16:36 +0300

    Set up the form

---
+++ validate.js
@@ -0,0 +1,18 @@
+function ValidateEmail(input) {
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (input.value.match(validRegex)) {
+    alert("Valid email address!");
+    document.form1.text1.focus();
+    return true;
+  } else {
+    alert("Invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
+}
+function validatePassword(){
+    var passRegex=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+    
+}
